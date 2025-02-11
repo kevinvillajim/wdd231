889a062277aaeff32b359c20e0850301f4495dea
kevinvillajim <kevinvillajim@hotmail.com>
2025-02-11 14:06:14 -0500

    Activity week 5 ended

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,18 +1,26 @@
 document.getElementById("timestamp").value = new Date().toISOString();
 
+const menuTogle = document.querySelector("#menuToggle");
+const nav = document.querySelector("#navigation");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  menuToggle.classList.toggle("open");
+});
+
 document.querySelectorAll(".modal-link").forEach((link) => {
-	link.addEventListener("click", (e) => {
-		e.preventDefault();
-		const modalId = link.id + "-modal"; // Get modal ID dynamically
-		const modal = document.getElementById(modalId);
-		if (modal) {
-			modal.showModal();
-		}
-	});
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const modalId = link.id + "-modal"; // Get modal ID dynamically
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.showModal();
+    }
+  });
 });
 
 document.querySelectorAll(".close-modal").forEach((button) => {
-	button.addEventListener("click", () => {
-		button.closest("dialog").close();
-	});
+  button.addEventListener("click", () => {
+    button.closest("dialog").close();
+  });
 });
